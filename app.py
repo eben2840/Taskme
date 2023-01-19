99888a3699c5529d289e9fe6984f10ecc808e1c2
eben2840 <ebenmills200@gmail.com>
2023-01-19 11:02:19 +0000

    noone

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,redirect,url_for,render_template,request
 app=Flask(__name__)
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
 def home():
     if request.method=='POST':
         # Handle POST Request here
@@ -12,14 +12,29 @@ def home():
 
 
 
+@app.route('/calender',methods=['GET','POST'])
+def calender():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('calender.html')
+    return render_template('calender.html')
 
-@app.route('/about',methods=['GET','POST'])
+
+
+@app.route('/',methods=['GET','POST'])
 def about():
     if request.method=='POST':
         # Handle POST Request here
         return render_template('about.html')
     return render_template('about.html')
 
+@app.route('/task',methods=['GET','POST'])
+def task():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('task.html')
+    return render_template('task.html')
+
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
